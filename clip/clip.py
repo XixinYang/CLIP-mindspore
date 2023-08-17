91762b66fbe07a91dc608586add58441ede3819b
XixinYang <yangxixin@huawei.com>
2023-08-17 11:57:24 +0800

    CLIP-mindspore version

--- clip/clip.py
+++ clip/clip.py
@@ -5,24 +5,24 @@ import warnings
 from typing import Any, Union, List
 from pkg_resources import packaging
 
-import torch
+import mindspore as ms
 from PIL import Image
-from torchvision.transforms import Compose, Resize, CenterCrop, ToTensor, Normalize
+from mindspore.dataset.vision import Resize, CenterCrop, ToTensor, Normalize, Decode, ToPIL
+from mindspore.dataset.transforms import Compose
 from tqdm import tqdm
+from mindspore import Tensor, load_checkpoint
 
 from .model import build_model
 from .simple_tokenizer import SimpleTokenizer as _Tokenizer
 
 try:
-    from torchvision.transforms import InterpolationMode
-    BICUBIC = InterpolationMode.BICUBIC
+    from mindspore.dataset.vision import Inter
+    BICUBIC = Inter.BICUBIC
 except ImportError:
     BICUBIC = Image.BICUBIC
 
-
-if packaging.version.parse(torch.__version__) < packaging.version.parse("1.7.1"):
-    warnings.warn("PyTorch version 1.7.1 or higher is recommended")
-
+if packaging.version.parse(ms.__version__) < packaging.version.parse("2.0.0"):
+    warnings.warn("MindSpore version 2.0.0 or higher is recommended")
 
 __all__ = ["available_models", "load", "tokenize"]
 _tokenizer = _Tokenizer()
@@ -57,7 +57,8 @@ def _download(url: str, root: str):
             warnings.warn(f"{download_target} exists, but the SHA256 checksum does not match; re-downloading the file")
 
     with urllib.request.urlopen(url) as source, open(download_target, "wb") as output:
-        with tqdm(total=int(source.info().get("Content-Length")), ncols=80, unit='iB', unit_scale=True, unit_divisor=1024) as loop:
+        with tqdm(total=int(source.info().get("Content-Length")), ncols=80, unit='iB', unit_scale=True,
+                  unit_divisor=1024) as loop:
             while True:
                 buffer = source.read(8192)
                 if not buffer:
@@ -78,131 +79,77 @@ def _convert_image_to_rgb(image):
 
 def _transform(n_px):
     return Compose([
+        ToPIL(),
         Resize(n_px, interpolation=BICUBIC),
         CenterCrop(n_px),
         _convert_image_to_rgb,
         ToTensor(),
-        Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711)),
+        Normalize([0.48145466, 0.4578275, 0.40821073], [0.26862954, 0.26130258, 0.27577711], is_hwc=False),
     ])
 
+
+# def _transform(n_px,image):
+#     if isinstance(image,Image.Image):
+#         return Compose([
+#             Resize(n_px, interpolation=BICUBIC),
+#             CenterCrop(n_px),
+#             _convert_image_to_rgb,
+#             ToTensor(),
+#             Normalize([0.48145466, 0.4578275, 0.40821073], [0.26862954, 0.26130258, 0.27577711], is_hwc=False),
+#         ])
+#     #判断输入是否为image格式，是的话就不用ToPIL()，否则其他就加ToPIL()
+#     else:
+#         return Compose([
+#             ToPIL(),
+#             Resize(n_px, interpolation=BICUBIC),
+#             CenterCrop(n_px),
+#             _convert_image_to_rgb,
+#             ToTensor(),
+#             Normalize([0.48145466, 0.4578275, 0.40821073], [0.26862954, 0.26130258, 0.27577711], is_hwc=False),
+#         ])
 
 def available_models() -> List[str]:
     """Returns the names of available CLIP models"""
     return list(_MODELS.keys())
 
 
-def load(name: str, device: Union[str, torch.device] = "cuda" if torch.cuda.is_available() else "cpu", jit: bool = False, download_root: str = None):
+def load(path: str, device: str = "Ascend", mode: int = 1):
     """Load a CLIP model
 
     Parameters
     ----------
-    name : str
-        A model name listed by `clip.available_models()`, or the path to a model checkpoint containing the state_dict
+    path : str
+        A model name listed by `clip.available_models()`, or the path to a model checkpoint containing the parameter_dict
 
-    device : Union[str, torch.device]
-        The device to put the loaded model
+    device : str
+        The device to put the loaded model, must be one of CPU, GPU, Ascend
 
-    jit : bool
+    mode : int
         Whether to load the optimized JIT model or more hackable non-JIT model (default).
-
-    download_root: str
-        path to download the model files; by default, it uses "~/.cache/clip"
+        If yes, GRAPH_MODE is supposed to be used, set mode to be 0;
+        else use PYNATIVE_MODE and set mode to be 1.
 
     Returns
     -------
-    model : torch.nn.Module
+    model : mindspore.nn.Cell
         The CLIP model
 
-    preprocess : Callable[[PIL.Image], torch.Tensor]
-        A torchvision transform that converts a PIL image into a tensor that the returned model can take as its input
+    preprocess : Callable[[PIL.Image], mindspore.Tensor]
+        A mindspore vision transform that converts a PIL image into a tensor that the returned model can take as its input
     """
-    if name in _MODELS:
-        model_path = _download(_MODELS[name], download_root or os.path.expanduser("~/.cache/clip"))
-    elif os.path.isfile(name):
-        model_path = name
+    ms.set_context(device_target=device, mode=mode)
+    if os.path.isfile(path):
+        ckp_dict = load_checkpoint(path)
     else:
-        raise RuntimeError(f"Model {name} not found; available models = {available_models()}")
+        raise RuntimeError(f"Model {path} not found; available models = {available_models()}")
 
-    with open(model_path, 'rb') as opened_file:
-        try:
-            # loading JIT archive
-            model = torch.jit.load(opened_file, map_location=device if jit else "cpu").eval()
-            state_dict = None
-        except RuntimeError:
-            # loading saved state dict
-            if jit:
-                warnings.warn(f"File {model_path} is not a JIT archive. Loading as a state dict instead")
-                jit = False
-            state_dict = torch.load(opened_file, map_location="cpu")
-
-    if not jit:
-        model = build_model(state_dict or model.state_dict()).to(device)
-        if str(device) == "cpu":
-            model.float()
-        return model, _transform(model.visual.input_resolution)
-
-    # patch the device names
-    device_holder = torch.jit.trace(lambda: torch.ones([]).to(torch.device(device)), example_inputs=[])
-    device_node = [n for n in device_holder.graph.findAllNodes("prim::Constant") if "Device" in repr(n)][-1]
-
-    def _node_get(node: torch._C.Node, key: str):
-        """Gets attributes of a node which is polymorphic over return type.
-        
-        From https://github.com/pytorch/pytorch/pull/82628
-        """
-        sel = node.kindOf(key)
-        return getattr(node, sel)(key)
-
-    def patch_device(module):
-        try:
-            graphs = [module.graph] if hasattr(module, "graph") else []
-        except RuntimeError:
-            graphs = []
-
-        if hasattr(module, "forward1"):
-            graphs.append(module.forward1.graph)
-
-        for graph in graphs:
-            for node in graph.findAllNodes("prim::Constant"):
-                if "value" in node.attributeNames() and str(_node_get(node, "value")).startswith("cuda"):
-                    node.copyAttributes(device_node)
-
-    model.apply(patch_device)
-    patch_device(model.encode_image)
-    patch_device(model.encode_text)
-
-    # patch dtype to float32 on CPU
-    if str(device) == "cpu":
-        float_holder = torch.jit.trace(lambda: torch.ones([]).float(), example_inputs=[])
-        float_input = list(float_holder.graph.findNode("aten::to").inputs())[1]
-        float_node = float_input.node()
-
-        def patch_float(module):
-            try:
-                graphs = [module.graph] if hasattr(module, "graph") else []
-            except RuntimeError:
-                graphs = []
-
-            if hasattr(module, "forward1"):
-                graphs.append(module.forward1.graph)
-
-            for graph in graphs:
-                for node in graph.findAllNodes("aten::to"):
-                    inputs = list(node.inputs())
-                    for i in [1, 2]:  # dtype can be the second or third argument to aten::to()
-                        if _node_get(inputs[i].node(), "value") == 5:
-                            inputs[i].node().copyAttributes(float_node)
-
-        model.apply(patch_float)
-        patch_float(model.encode_image)
-        patch_float(model.encode_text)
-
-        model.float()
-
-    return model, _transform(model.input_resolution.item())
+    model = build_model(ckp_dict)
+    if str(device).lower() == "cpu":
+        model.to_float(ms.float32)
+    return model, _transform(model.visual.input_resolution)
 
 
-def tokenize(texts: Union[str, List[str]], context_length: int = 77, truncate: bool = False) -> Union[torch.IntTensor, torch.LongTensor]:
+def tokenize(texts: Union[str, List[str]], context_length: int = 77, truncate: bool = False) -> Tensor:
     """
     Returns the tokenized representation of given input string(s)
 
@@ -220,7 +167,6 @@ def tokenize(texts: Union[str, List[str]], context_length: int = 77, truncate: b
     Returns
     -------
     A two-dimensional tensor containing the resulting tokens, shape = [number of input strings, context_length].
-    We return LongTensor when torch version is <1.8.0, since older index_select requires indices to be long.
     """
     if isinstance(texts, str):
         texts = [texts]
@@ -228,10 +174,7 @@ def tokenize(texts: Union[str, List[str]], context_length: int = 77, truncate: b
     sot_token = _tokenizer.encoder["<|startoftext|>"]
     eot_token = _tokenizer.encoder["<|endoftext|>"]
     all_tokens = [[sot_token] + _tokenizer.encode(text) + [eot_token] for text in texts]
-    if packaging.version.parse(torch.__version__) < packaging.version.parse("1.8.0"):
-        result = torch.zeros(len(all_tokens), context_length, dtype=torch.long)
-    else:
-        result = torch.zeros(len(all_tokens), context_length, dtype=torch.int)
+    result = ms.ops.zeros((len(all_tokens), context_length), dtype=ms.int32)
 
     for i, tokens in enumerate(all_tokens):
         if len(tokens) > context_length:
@@ -240,6 +183,6 @@ def tokenize(texts: Union[str, List[str]], context_length: int = 77, truncate: b
                 tokens[-1] = eot_token
             else:
                 raise RuntimeError(f"Input {texts[i]} is too long for context length {context_length}")
-        result[i, :len(tokens)] = torch.tensor(tokens)
+        result[i, :len(tokens)] = Tensor(tokens)
 
     return result
